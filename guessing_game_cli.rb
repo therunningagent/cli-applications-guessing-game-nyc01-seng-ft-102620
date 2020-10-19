875f5d5c275e7fc65833c4c5863c49856007f4fb
Luis Torres <luistjr@gmail.com>
2020-10-19 14:13:02 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,10 +4,16 @@ def random_number
   rand(6) + 1 
 end 
 
+def guess_number
+  puts "Please guess a number between 1 and 6."
+end 
+
 def get_user_input
   gets.chomp
 end 
 
 def run_guessing_game
-
+  store_random_number = random_number
+  guess_number
+  get_user_input
 end 
