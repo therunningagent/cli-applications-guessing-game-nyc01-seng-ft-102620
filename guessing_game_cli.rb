4d4f17cecb86556dcbd0bbadb90cea00612f9202
Luis Torres <luistjr@gmail.com>
2020-10-19 14:27:35 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -10,7 +10,6 @@ def run_guessing_game
     
   elsif user_input != random_number 
     puts "/Sorry! The computer guessed #{random_number}./"
-    
   else
     puts "/You guessed the correct number!/"
  end 
