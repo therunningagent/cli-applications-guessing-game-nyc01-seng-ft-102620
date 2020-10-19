e971a23c1a39b31af125150c357249b627fa0bf7
Luis Torres <luistjr@gmail.com>
2020-10-19 14:23:26 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -19,4 +19,11 @@ def run_guessing_game
   random_number = rand(6) + 1 
   user_input = gets.chomp 
   
+  if user_input == random_number 
+    puts "You guessed the correct number!"
+  elsif user_input =! random_number
+    puts "Sorry! Theh computer guessed #{random_number}."
+  else user_input == "exit"
+    puts "Goodbye!"
+  end 
 end 
